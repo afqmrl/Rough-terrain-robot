026e7dd01b374d661b9bbf0361f5fbd2e264b7c5
afqmrl <152699285+afqmrl@users.noreply.github.com>
2024-04-19 11:58:51 +0100

    Update charging probe alignment.ino

--- Source/charging probe alignment.ino
+++ Source/charging probe alignment.ino
@@ -1,5 +1,25 @@
-#include <Arduino.h>
+#include <s3servo.h>
 
-int main() {
-    // Charging probe alignment code here.
+static const int servoPin = 4;
+
+s3servo servo;
+
+void setup() {
+    Serial.begin(115200);
+    servo.attach(servoPin);
+    //servo.attach(servoPin,0,0,180,500,2000); 
+}
+
+void loop() {
+    for(int angle = 0; angle <= 180; angle++) {
+        servo.write(angle);
+        Serial.println(angle);
+        delay(20);
+    }
+
+    for(int angle = 180; angle >= 0; angle--) {
+        servo.write(angle);
+        Serial.println(angle);
+        delay(20);
+    }
 }
